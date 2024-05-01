eab6e152cf02be7cf24e9d67beddb4153c973e72
neurlang <77860779+neurlang@users.noreply.github.com>
2024-05-01 10:34:42 +0200

    Fix the inference

--- cmd/infer_is_alnum/main.go
+++ cmd/infer_is_alnum/main.go
@@ -4,13 +4,13 @@ import "github.com/neurlang/classifier/inference"
 
 type Program [][2]uint32
 
-func (p *Program) Len() int {
-	return len(*p)
+func (p Program) Len() int {
+	return len(p)
 }
-func (p *Program) Get(n int) (uint32, uint32) {
-	return (*p)[n][0], (*p)[n][1]
+func (p Program) Get(n int) (uint32, uint32) {
+	return (p)[n][0], (p)[n][1]
 }
-func (p *Program) Xor() uint32 {
+func (p Program) Bits() byte {
 	return 0 // unused, return 0
 }
 
@@ -19,7 +19,7 @@ func main() {
 
 	for i := ' '; i < '~'; i++ {
 
-		println(i, string([]rune{rune(i)}), inference.BoolInfer(uint32(i), &model))
+		println(i, string([]rune{rune(i)}), inference.BoolInfer(uint32(i), model))
 
 	}
 }

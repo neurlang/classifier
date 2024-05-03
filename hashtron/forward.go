334ed0bfcd0c27d99c80484984c3a009bb23c870
neurlang <77860779+neurlang@users.noreply.github.com>
2024-05-03 21:24:48 +0200

    add hashtron forward pass

---
+++ hashtron/forward.go
@@ -0,0 +1,27 @@
+package hashtron
+
+import "github.com/neurlang/classifier/hash"
+
+func (h Hashtron) Forward(command uint32, negate bool) (out uint16) {
+	if h.Len() == 0 {
+		return
+	}
+	for j := byte(0); j < h.Bits(); j++ {
+		var input = uint32(command) | uint32(1 << (j + 16))
+		var ss, maxx = h.Get(0)
+		input = hash.Hash(input, ss, maxx)
+		for i := 1; i < h.Len(); i++ {
+			var s, max = h.Get(i)
+			maxx -= max
+			input = hash.Hash(input, s, maxx)
+		}
+		input &= 1
+		if negate {
+			input ^= 1
+		}
+		if input != 0 {
+			out |= 1 << j
+		}
+	}
+	return
+}

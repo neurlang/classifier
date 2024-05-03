f14b852d27c4c16b86c856e19b402aa274763eb2
neurlang <77860779+neurlang@users.noreply.github.com>
2024-05-03 21:23:36 +0200

    Initialize hashtron with a random weights

--- hashtron/constructor.go
+++ hashtron/constructor.go
@@ -1,14 +1,21 @@
 package hashtron
 
 import "errors"
+import "math/rand"
 
 func New(program [][2]uint32, bits byte, future ...interface{}) (h *Hashtron, err error) {
 	if future != nil && len(future) > 0 {
 		return nil, errors.New("future arg not supported (new Hashtron)")
 	}
 	h = new(Hashtron)
-
-	h.program = program
+	if bits == 0 {
+		bits = 1
+	}
+	if program == nil {
+		h.program = [][2]uint32{{rand.Uint32()>>1, uint32(1)<<bits}}
+	} else {
+		h.program = program
+	}
 	h.bits = bits
 	return
 }

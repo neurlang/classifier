a9574914a62142eb11881b1849f3b331fe472e76
neurlang <77860779+neurlang@users.noreply.github.com>
2024-05-06 18:08:47 +0200

    Inference api refactor

--- inference/infer.go
+++ inference/infer.go
@@ -10,15 +10,28 @@ type Model interface {
 }
 
 func BoolInfer(input uint32, m Model) bool {
-	input = Uint32Infer(input, m)
+	input = uint32(uint64Infer(input, m))
 	input &= 1
 	return input != 0
 }
 
+// Reduced infer
 func Uint16Infer2(input uint16, m0, m1 Model) (out uint16) {
 	return Uint16Infer(Uint16Infer(input, m0), m1)
 }
-func Uint16Infer(command uint16, m Model) (out uint16) {
+func Uint16Infer(input uint16, m Model) (out uint16) {
+	return uint16(uint64Infer(uint32(input), m))
+}
+
+func Uint32Infer(input uint16, m Model) uint32 {
+	return uint32(uint64Infer(uint32(input), m))
+}
+
+func Uint64Infer(input uint16, m Model) (out uint64) {
+	return uint64Infer(uint32(input), m)
+}
+
+func uint64Infer(command uint32, m Model) (out uint64) {
 	if m.Len() == 0 {
 		return
 	}
@@ -37,18 +50,4 @@ func Uint16Infer(command uint16, m Model) (out uint16) {
 		}
 	}
 	return
-}
-
-func Uint32Infer(input uint32, m Model) uint32 {
-	if m.Len() == 0 {
-		return 0
-	}
-	var ss, maxx = m.Get(0)
-	input = hash.Hash(input, ss, maxx)
-	for i := 1; i < m.Len(); i++ {
-		var s, max = m.Get(i)
-		maxx -= max
-		input = hash.Hash(input, s, maxx)
-	}
-	return input
 }

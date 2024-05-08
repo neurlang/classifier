e6502227db7dda63b58fc2280ac95e814ea33aaf
neurlang <77860779+neurlang@users.noreply.github.com>
2024-05-08 20:54:47 +0200

    Tally

---
+++ datasets/tally.go
@@ -0,0 +1,91 @@
+package datasets
+
+type Tally struct {
+	// this is for multiway classification layers
+	// each input has a map of possible outputs with number of votes
+	// the highest vote in the inner map wins
+	mapping map[uint16]map[uint64]uint64
+
+	// these are votes in case when the feature caused correct overall result
+	// true value is added as +1, false value is voted as -1
+	// if the tally is positive we map the feature to true, false if negative
+	correct map[uint32]int64
+
+	// these are votes in case when the feature caused better result
+	// true value is added as +1, false value is voted as -1
+	// if the tally is positive we map the feature to true, false if negative
+	improve map[uint32]int64
+}
+
+// Init initializes the tally dataset structure
+func (t *Tally) Init() {
+	t.mapping = make(map[uint16]map[uint64]uint64)
+	t.correct = make(map[uint32]int64)
+	t.improve = make(map[uint32]int64)
+}
+
+// Improve votes for feature which improved the overall result
+func (t *Tally) AddToImprove(feature uint32, vote int8) {
+	if vote == 0 {
+		return
+	}
+	t.improve[feature] += int64(vote)
+	if t.improve[feature] == 0 {
+		delete(t.improve, feature)
+	}
+}
+
+// Correct votes for feature which caused the overall result to be correct
+func (t *Tally) AddToCorrect(feature uint32, vote int8) {
+	if vote == 0 {
+		return
+	}
+	t.correct[feature] += int64(vote)
+	if t.correct[feature] == 0 {
+		delete(t.correct, feature)
+	}
+}
+
+// AddToMapping adds feature maps to this output vote to mapping
+func (t *Tally) AddToMapping(feature uint16, output uint64) {
+	if t.mapping[feature] == nil {
+		t.mapping[feature] = make(map[uint64]uint64)
+	}
+	t.mapping[feature][output]++
+}
+
+// Split Splits the tally structure into a splitted dataset
+func (t *Tally) Split() SplittedDataset {
+	if len(t.mapping) > 0 {
+		var mapp Datamap
+		mapp.Init()
+		var suma = 0
+		for k, freq := range t.mapping {
+			var maxk uint64
+			for k2 := range freq {
+				if freq[k2] > freq[maxk] {
+					maxk = k2
+				} 
+			}
+			suma += int(freq[maxk])
+			mapp[k] = maxk
+		}
+		return mapp.Split()
+	} else {
+		var sett Dataset
+		sett.Init()
+		// we initialize the set with pairs which improve first
+		for value, rating := range t.improve {
+			if rating != 0 {
+				sett[value] = rating > 0
+			}
+		}
+		// finally we overwrite the set with pairs which make it correct
+		for value, rating := range t.correct {
+			if rating != 0 {
+				sett[value] = rating > 0
+			}
+		}
+		return sett.Split()
+	}
+}

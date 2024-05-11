e5ba4915fdd8197303258a6df23ca74aff8c0de1
neurlang <77860779+neurlang@users.noreply.github.com>
2024-05-11 16:35:21 +0200

    Move Tallying to FeedforwardNetwork

--- cmd/train_mnist/main.go
+++ cmd/train_mnist/main.go
@@ -10,7 +10,7 @@ import "github.com/neurlang/classifier/layer/conv2d"
 import "github.com/neurlang/classifier/layer/majpool2d"
 import "github.com/neurlang/classifier/net/feedforward"
 
-func error_abs(a,b uint16) uint16 {
+func error_abs(a, b uint32) uint32 {
 	if a > b {
 		return a - b
 	}
@@ -22,18 +22,18 @@ func main() {
 		panic(err.Error())
 	}
 	var net feedforward.FeedforwardNetwork
-	const l0Dim = mnist.ImgSize-1
-	const l1Dim = mnist.SmallImgSize-1
+	const l0Dim = mnist.ImgSize - 1
+	const l1Dim = mnist.SmallImgSize - 1
 	net.NewLayer(l0Dim*l0Dim, 0)
 	net.NewCombiner(conv2d.MustNew(27, 27, 16, 16, 1))
 	net.NewLayer(l1Dim*l1Dim, 0)
 	net.NewCombiner(majpool2d.MustNew(3, 3, 4, 4, 1))
 	net.NewLayer(1, 4)
-	
+
 	//Load(net)
-	
+
 	//println(net.GetHashtron(1305))
-	
+
 	trainWorst := func(worst int) {
 		var tally datasets.Tally
 		tally.Init()
@@ -43,116 +43,12 @@ func main() {
 
 			wg.Add(1)
 			go func(jj int) {
-			var input = mnist.Input(mnist.InferSet[jj])
-			var output = uint16(mnist.InferLabels[jj])
+				var input = mnist.Input(mnist.InferSet[jj])
+				var output = feedforward.SingleValue(mnist.InferLabels[jj])
 
-
-
-			switch net.GetLayer(worst) {
-			case 4: // layer 2
-				inter, _ := net.Forward(&input, 0, -1, 0)
-				inter, _ = net.Forward(inter, 2, -1, 0)
-
-				ifeature := uint16(inter.Feature(0))
-
-				tally.AddToMapping(ifeature, uint64(output))
-
-			case 2: // layer 1
-				var predicted [2]uint16
-				var compute [2]int8
-				
-				var inter, _ = net.Forward(&input, 0, -1, 0)
-				ifw := input.Feature(net.GetPosition(worst))
-				for neg := 0; neg < 2; neg++ {
-					inter, computed := net.Forward(inter, 2, net.GetPosition(worst), neg)
-					if computed {
-						compute[neg] = 1
-					} else {
-						compute[neg] = -1
-					}
-					if neg == 0 {
-						if inter.Disregard(net.GetLayerPosition(2, worst)) {
-							goto end
-						}
-					}
-					inter, _ = net.Forward(inter, 4, -1, 0)
-					predicted[neg] = uint16(inter.Feature(0))
-				}
-				//fmt.Println(predicted, output)
-				if (predicted[0] == output) && (predicted[1] == output) {
-					// we are correct anyway
-					goto end
-				}
-				for neg := 0; neg < 2; neg++ {
-					if predicted[neg] == output {
-
-						tally.AddToCorrect(ifw, compute[neg])
-						// shift to correct output
-
-						goto end
-					}
-				}
-				var minneg int
-				for neg := 0; neg < 2; neg++ {
-					if error_abs(predicted[neg], output) < error_abs(predicted[minneg], output) {
-						minneg = neg
-					}
-				}
-				
-				// shift away to minneg output
-				tally.AddToImprove(ifw, compute[minneg])
-				
-			case 0: // layer 0
-				var predicted [2]uint16
-				var compute [2]int8
-				
-				//var inter, computed = net.Forward(&input, 0, -1, 0)
-				ifw := input.Feature(net.GetPosition(worst))
-				//if _, ok := tabu[ifw]; ok {
-				//	continue
-				//}
-				for neg := 0; neg < 2; neg++ {
-					var inter, computed = net.Forward(&input, 0, net.GetPosition(worst), neg)
-					if computed {
-						compute[neg] = 1
-					} else {
-						compute[neg] = -1
-					}
-					if neg == 0 {
-						if inter.Disregard(net.GetLayerPosition(0, worst)) {
-							goto end
-						}
-					}
-					inter, _ = net.Forward(inter, 2, -1, 0)
-					inter, _ = net.Forward(inter, 4, -1, 0)
-					predicted[neg] = uint16(inter.Feature(0))
-				}
-				//fmt.Println(predicted, output)
-				if (predicted[0] == output) && (predicted[1] == output) {
-					// we are correct anyway
-					goto end
-				}
-				for neg := 0; neg < 2; neg++ {
-					if predicted[neg] == output {
-					
-						tally.AddToCorrect(ifw, compute[neg])
-						// shift to correct output
-
-						goto end
-					}
-				}
-				var minneg int
-				for neg := 0; neg < 2; neg++ {
-					if error_abs(predicted[neg], output) < error_abs(predicted[minneg], output) {
-						minneg = neg
-					}
-				}
-				
-				// shift away to minneg output
-				tally.AddToImprove(ifw, compute[minneg])
-				
-			}
-end:
+				net.Tally(&input, &output, worst, tally, func(i, j feedforward.FeedforwardNetworkInput) bool {
+					return error_abs(i.Feature(0), output.Feature(0)) < error_abs(j.Feature(0), output.Feature(0))
+				})
 				wg.Done()
 
 			}(jj)
@@ -179,7 +75,7 @@ end:
 		h.Printer = 70
 
 		// save any solution to disk
-		h.InitialLimit = 1000+4*tally.Len()
+		h.InitialLimit = 1000 + 4*tally.Len()
 		h.EndWhenSolved = true
 
 		h.Name = fmt.Sprint(worst)
@@ -204,20 +100,12 @@ end:
 		for i, v := range [2][]byte{mnist.TrainLabels, mnist.InferLabels} {
 			for j := range v {
 				var input = mnist.Input(mnist.TrainSet[j])
-				var output = uint16(mnist.TrainLabels[j])
+				var output = uint32(mnist.TrainLabels[j])
 				if i == 1 {
 					input = mnist.Input(mnist.InferSet[j])
-					output = uint16(mnist.InferLabels[j])
+					output = uint32(mnist.InferLabels[j])
 				}
-				var predicted uint16
-				{
-					//inter, _ := net.Forward(&input, 0, -1, 0)
-					inter, _ := net.Forward(&input, 0, -1, 0)
-					inter, _ = net.Forward(inter, 2, -1, 0)
-					inter, _ = net.Forward(inter, 4, -1, 0)
-					predicted = uint16(inter.Feature(0))
-
-				}
+				var predicted = net.Infer(&input).Feature(0)
 				if predicted == output {
 					quality[i]++
 				} else {
--- net/feedforward/feedforward_network.go
+++ net/feedforward/feedforward_network.go
@@ -2,6 +2,7 @@ package feedforward
 
 import "github.com/neurlang/classifier/hashtron"
 import "github.com/neurlang/classifier/layer"
+import "github.com/neurlang/classifier/datasets"
 import "sync"
 
 type Intermediate interface {
@@ -25,8 +26,8 @@ type FeedforwardNetworkInput interface {
 }
 
 type FeedforwardNetwork struct {
-	layers [][]hashtron.Hashtron
-	mapping []bool
+	layers    [][]hashtron.Hashtron
+	mapping   []bool
 	combiners []layer.Layer
 }
 
@@ -35,6 +36,10 @@ func (f FeedforwardNetwork) Len() (o int) {
 		o += len(v)
 	}
 	return
+}
+
+func (f FeedforwardNetwork) LenLayers() int {
+	return len(f.layers)
 }
 
 func (f FeedforwardNetwork) GetLayer(n int) int {
@@ -92,19 +97,25 @@ func (f *FeedforwardNetwork) NewLayer(n int, bits byte) {
 	f.combiners = append(f.combiners, nil)
 }
 
-
 func (f *FeedforwardNetwork) NewCombiner(layer layer.Layer) {
 	f.layers = append(f.layers, nil)
 	f.mapping = append(f.mapping, false)
 	f.combiners = append(f.combiners, layer)
 }
 
-func (f FeedforwardNetwork) IsMapLayerOf(n int) (bool) {
+func (f FeedforwardNetwork) IsMapLayerOf(n int) bool {
 	if f.GetLayer(n) == -1 {
 		return false
 	}
 	return f.mapping[f.GetLayer(n)]
 }
+func (f FeedforwardNetwork) Infer(in FeedforwardNetworkInput) (ouput FeedforwardNetworkInput) {
+	for l_prev := 0; l_prev < f.LenLayers(); l_prev += 2 {
+		in, _ = f.Forward(in, l_prev, -1, 0)
+	}
+	return in
+}
+
 func (f FeedforwardNetwork) Forward(in FeedforwardNetworkInput, l, worst, neg int) (inter Intermediate, computed bool) {
 	if len(f.combiners) > l+1 && f.combiners[l+1] != nil {
 		var combiner = f.combiners[l+1].Lay()
@@ -113,9 +124,9 @@ func (f FeedforwardNetwork) Forward(in FeedforwardNetworkInput, l, worst, neg in
 			wg.Add(1)
 			go func(i int) {
 				var bit = f.layers[l][i].Forward(in.Feature(i), (i == worst) && (neg == 1))
-				combiner.Put(i, bit & 1 != 0)
+				combiner.Put(i, bit&1 != 0)
 				if i == worst {
-					computed = bit & 1 != 0
+					computed = bit&1 != 0
 				}
 				wg.Done()
 			}(i)
@@ -131,4 +142,70 @@ func (f FeedforwardNetwork) Forward(in FeedforwardNetworkInput, l, worst, neg in
 		return SingleValue(bit & 1), (bit & 1) != 0
 	}
 	return nil, false
+}
+
+func (f *FeedforwardNetwork) Tally(in, output FeedforwardNetworkInput, worst int, tally datasets.Tally, less func(i, j FeedforwardNetworkInput) bool) {
+	l := f.GetLayer(worst)
+	if len(f.combiners) > l+1 && f.combiners[l+1] != nil {
+		var predicted [2]FeedforwardNetworkInput
+		var compute [2]int8
+
+		for l_prev := 0; l_prev < l; l_prev += 2 {
+			in, _ = f.Forward(in, l_prev, -1, 0)
+		}
+		ifw := in.Feature(f.GetPosition(worst))
+		for neg := 0; neg < 2; neg++ {
+			inter, computed := f.Forward(in, l, f.GetPosition(worst), neg)
+			if computed {
+				compute[neg] = 1
+			} else {
+				compute[neg] = -1
+			}
+			if neg == 0 {
+				if inter.Disregard(f.GetLayerPosition(l, worst)) {
+					return
+				}
+			}
+			for l_post := l + 2; l_post < f.LenLayers(); l_post += 2 {
+				in, _ = f.Forward(in, l_post, -1, 0)
+			}
+			predicted[neg] = in
+		}
+		if !less(predicted[0], output) && !less(predicted[1], output) &&
+			!less(output, predicted[0]) && !less(output, predicted[1]) {
+			// we are correct anyway
+			return
+		}
+		for neg := 0; neg < 2; neg++ {
+			if !less(predicted[neg], output) && !less(output, predicted[neg]) {
+
+				tally.AddToCorrect(ifw, compute[neg])
+				// shift to correct output
+
+				return
+			}
+		}
+		if less(predicted[0], predicted[1]) {
+			// shift away to less wrong output
+			tally.AddToImprove(ifw, compute[0])
+		} else {
+			// shift away to less wrong output
+			tally.AddToImprove(ifw, compute[1])
+		}
+	}
+	if len(f.mapping) > l && f.mapping[l] {
+		for l_prev := 0; l_prev < l; l_prev += 2 {
+			in, _ = f.Forward(in, l_prev, -1, 0)
+		}
+		ifeature := uint16(in.Feature(0))
+
+		tally.AddToMapping(ifeature, uint64(output.Feature(0)))
+	} else {
+		for l_prev := 0; l_prev < l; l_prev += 2 {
+			in, _ = f.Forward(in, l_prev, -1, 0)
+		}
+		ifeature := uint32(in.Feature(0))
+
+		tally.AddToCorrect(ifeature, 1)
+	}
 }

ab8ef95d7fdc5a8ea3358af55bc08555859bc20f
neurlang <77860779+neurlang@users.noreply.github.com>
2024-05-12 06:47:00 +0200

    fix majority pool

--- layer/majpool2d/combiner.go
+++ layer/majpool2d/combiner.go
@@ -5,22 +5,22 @@ func (s *MajPool2D) Put(n int, v bool) {
 	s.vec[n] = v
 }
 
-
 // Disregard tells whether putting value false at position n would not affect
 // any feature output (as opposed to putting value true at position n).
 func (s *MajPool2D) Disregard(n int) bool {
 	orign := n
-	matrix := s.width * s.height * s.subwidth * s.subheight
+	submatrix := s.subwidth * s.subheight
+	matrix := s.width * s.height * submatrix
 	base := (n / matrix) * matrix
 	n %= matrix
-	n /= (s.subwidth * s.subheight)
-	n *= (s.subwidth * s.subheight)
+	n /= submatrix
+	n *= submatrix
 	var w int
-	for m := 0; m < int(s.subheight)*int(s.subwidth); m++ {
-		if orign == base + m + n {
+	for m := 0; m < submatrix; m++ {
+		if orign == base+m+n {
 			continue
 		}
-		if (s.vec)[base + m + n] {
+		if (s.vec)[base+m+n] {
 			w++
 		} else {
 			w--
@@ -31,27 +31,27 @@ func (s *MajPool2D) Disregard(n int) bool {
 
 // Feature returns the m-th feature from the combiner.
 func (s *MajPool2D) Feature(m int) (o uint32) {
-	matrix := s.width * s.height
+	supermatrix := s.width * s.height
 	submatrix := s.subwidth * s.subheight
-	base := (m / matrix) * submatrix
-	m %= matrix
-	y := m / s.width
-	x := m % s.width
-	subx := submatrix*x
-	suby := submatrix * s.width * y
-	for n := 0; n < submatrix; n++ {
-		var w int
-		for m := 0; m < int(s.subheight)*int(s.subwidth); m++ {
-			if (s.vec)[base + subx + suby + n] {
-				w++
-			} else {
-				w--
+	residualwidth := (s.width - s.capwidth + 1)
+	matrix := supermatrix * submatrix
+	base := (m / matrix) * matrix
+	starty := m / residualwidth
+	startx := m % residualwidth
+	for y := 0; y < s.capheight; y++ {
+		for x := 0; x < s.capwidth; x++ {
+			var w int
+			for m := 0; m < submatrix; m++ {
+				if (s.vec)[base+submatrix*(s.width*(y+starty)+(x+startx))+m] {
+					w++
+				} else {
+					w--
+				}
+			}
+			if w > 0 {
+				o |= 1 << (s.capwidth*y + x)
 			}
 		}
-		if w > 0 {
-			o |= 1 << n
-		}
 	}
-	return 
+	return
 }
-
--- layer/majpool2d/layer.go
+++ layer/majpool2d/layer.go
@@ -4,18 +4,18 @@ import "github.com/neurlang/classifier/layer"
 import "fmt"
 
 type MajPool2DLayer struct {
-	width, height, subwidth, subheight, repeat int
+	width, height, subwidth, subheight, capwidth, capheight, repeat int
 }
 
 type MajPool2D struct {
-	vec []bool
-	width, height, subwidth, subheight, repeat int
+	vec                                                             []bool
+	width, height, subwidth, subheight, capwidth, capheight, repeat int
 }
 
 // New creates a new MajPool2D layer with size, subsize and repeat
-func New(width, height, subwidth, subheight, repeat int) (o *MajPool2DLayer, err error) {
+func New(width, height, subwidth, subheight, capwidth, capheight, repeat int) (o *MajPool2DLayer, err error) {
 	submatrix := subwidth * subheight
-	if submatrix & 1 == 0 {
+	if submatrix&1 == 0 {
 		return nil, fmt.Errorf("even matrix")
 	}
 	o = new(MajPool2DLayer)
@@ -23,13 +23,15 @@ func New(width, height, subwidth, subheight, repeat int) (o *MajPool2DLayer, err
 	o.height = height
 	o.subwidth = subwidth
 	o.subheight = subheight
+	o.capwidth = capwidth
+	o.capheight = capheight
 	o.repeat = repeat
 	return
 }
 
 // MustNew creates a new MajPool2D layer with size, subsize and repeat
-func MustNew(width, height, subwidth, subheight, repeat int) (*MajPool2DLayer) {
-	o, err := New(width, height, subwidth, subheight, repeat)
+func MustNew(width, height, subwidth, subheight, capwidth, capheight, repeat int) *MajPool2DLayer {
+	o, err := New(width, height, subwidth, subheight, capwidth, capheight, repeat)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -37,13 +39,15 @@ func MustNew(width, height, subwidth, subheight, repeat int) (*MajPool2DLayer) {
 }
 
 // Lay turns MajPool2D layer into a combiner
-func (i *MajPool2DLayer) Lay() (layer.Combiner) {
+func (i *MajPool2DLayer) Lay() layer.Combiner {
 	var o MajPool2D
-	o.vec = make([]bool, i.width * i.height * i.subwidth * i.subheight * i.repeat)
+	o.vec = make([]bool, i.width*i.height*i.subwidth*i.subheight*i.repeat)
 	o.width = i.width
 	o.height = i.height
 	o.subwidth = i.subwidth
 	o.subheight = i.subheight
+	o.capwidth = i.capwidth
+	o.capheight = i.capheight
 	o.repeat = i.repeat
 	return &o
 }

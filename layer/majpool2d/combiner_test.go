2d5b81ecd5fa06e79681fdd7985ae4ac981ad5e2
neurlang <77860779+neurlang@users.noreply.github.com>
2025-01-15 16:54:10 +0100

    add test to test the new feature

---
+++ layer/majpool2d/combiner_test.go
@@ -0,0 +1,55 @@
+package majpool2d
+
+import rand "math/rand/v2"
+
+import (
+	"testing"
+)
+
+// every passes test
+func TestEvery(t *testing.T) {
+	const fanout1 = 1
+	const fanout2 = 13
+	const fanout3 = 1
+	const fanout4 = 13
+	
+	a := MustNew2(1, fanout1*fanout2*fanout4, 1, fanout3, 1, fanout4, 1, 0)
+
+outer:
+	for {
+
+		var combiner = a.Lay()
+		var q = rand.IntN(fanout1*fanout2*fanout4)
+
+		combiner.Put(q, true)
+		
+		println("put at", q)
+
+		for j := 0; j < fanout2; j++ {
+			if combiner.Feature(j) != 0 {
+				println(q)
+				break outer
+			}
+		}
+		panic("repeatinga")
+	}
+	b := MustNew2(1, fanout2, 1, fanout1, 1, fanout2, 1, 0)
+outer2:
+	for {
+
+		var combiner = b.Lay()
+		var q = rand.IntN(fanout2)
+
+		combiner.Put(q, true)
+		
+		println("put at", q)
+
+		for j := 0; j < 1; j++ {
+			if combiner.Feature(j) != 0 {
+				println(q)
+				break outer2
+			}
+		}
+		panic("repeatingb")
+	}
+}

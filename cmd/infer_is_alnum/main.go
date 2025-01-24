663c7fa1833ef66a4e7f120c541af53053c9fb6c
neurlang <77860779+neurlang@users.noreply.github.com>
2025-01-24 16:03:25 +0100

    is alnum demo new

--- cmd/infer_is_alnum/main.go
+++ cmd/infer_is_alnum/main.go
@@ -1,26 +1,63 @@
-// Infer on Is Alnum dataset
 package main
 
-import "github.com/neurlang/classifier/inference"
+import "sync/atomic"
+//import "fmt"
+//import "runtime"
+//import "math"
+//import "math/rand"
+import "flag"
 
-type Program [][2]uint32
+//import "os"
+import "github.com/neurlang/classifier/datasets/isalnum"
+import "github.com/neurlang/classifier/net/feedforward"
+import "github.com/neurlang/classifier/parallel"
 
-func (p Program) Len() int {
-	return len(p)
-}
-func (p Program) Get(n int) (uint32, uint32) {
-	return (p)[n][0], (p)[n][1]
-}
-func (p Program) Bits() byte {
-	return 1 // unused, return 0
+func error_abs(a, b uint16) uint16 {
+	if a > b {
+		return a - b
+	}
+	return b - a
 }
 
 func main() {
-	var model = Program(program)
 
-	for i := ' '; i < '~'; i++ {
+	dstmodel := flag.String("dstmodel", "", "model destination .json.lzw file")
+	flag.Bool("pgo", false, "enable pgo")
+	resume := flag.Bool("resume", false, "resume training")
+	flag.Parse()
+	
+	const classes = 10
 
-		println(i, string([]rune{rune(i)}), inference.BoolInfer(uint32(i), model))
+	dataset := isalnum.Dataslice{}
+
+
+	var net feedforward.FeedforwardNetwork
+	net.NewLayer(1, 0)
+
+
+	evaluate := func() {
+		var percent, errsum atomic.Uint64
+		parallel.ForEach(dataset.Len(), 1000, func(j int) {
+			{
+				var io = dataset.Get(j)
+
+				var predicted = net.Infer2(&io) % classes
+				//println(predicted, io.Output())
+				if predicted == io.Output()%net.GetClasses() {
+					percent.Add(1)
+				}
+				errsum.Add(uint64(error_abs(predicted, io.Output())))
+			}
+		})
+		success := int(percent.Load()) * 100 / dataset.Len()
+		println("[success rate]", success, "%", "with", errsum.Load(), "errors")
 
 	}
+	if resume != nil && *resume && dstmodel != nil {
+		err := net.ReadZlibWeightsFromFile(*dstmodel)
+		if err != nil {
+			println(err.Error())
+		}
+	}
+	evaluate()
 }
--- cmd/train_is_alnum/main.go
+++ cmd/train_is_alnum/main.go
@@ -1,41 +1,152 @@
-// Train on Is Alnum dataset
 package main
 
+import "sync/atomic"
+import "fmt"
 import "runtime"
 
+//import "math"
+//import "math/rand"
+import "flag"
+import "os"
 import "github.com/neurlang/classifier/datasets/isalnum"
-import learning "github.com/neurlang/classifier/learning/avx"
+import "github.com/neurlang/classifier/datasets"
+import "github.com/neurlang/classifier/learning"
+import "github.com/neurlang/classifier/net/feedforward"
+import "github.com/neurlang/classifier/parallel"
+
+func error_abs(a, b uint32) uint32 {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
 
 func main() {
-	var h learning.HyperParameters
-	var dataset = isalnum.Dataslice{}.Set()
 
-	h.Threads = 2
-	//affects initial modulo
-	h.Factor = 3
+	dstmodel := flag.String("dstmodel", "", "model destination .json.lzw file")
+	flag.Bool("pgo", false, "enable pgo")
+	resume := flag.Bool("resume", false, "resume training")
+	flag.Parse()
+
+	var improved_success_rate = 0
+	var difficulty uint32 = 1
+
+	dataset := isalnum.Dataslice{}
+
+	// a net with one neuron
+	var net feedforward.FeedforwardNetwork
+	net.NewLayer(1, 0)
 	
-	h.AvxLanes = 16
-	//h.AvxSkip = 4
 
-	// shuffle before solving attempts
-	h.Shuffle = true
-	h.Seed = true
+	trainWorst := func(worst int) {
+		var tally = new(datasets.Tally)
+		tally.Init()
+		tally.SetFinalization(false)
 
-	// restart when stuck
-	h.DeadlineMs = 1000 * runtime.NumCPU() / 2
-	h.DeadlineRetry = 3
+		parallel.ForEach(dataset.Len(), 1000, func(jjj int) {
+			{
+				var io = dataset.Get(jjj)
 
-	// additional modulo reduction, affects solution size
-	h.Subtractor = 6
+				net.Tally4(&io, worst, tally, nil)
+			}
+		})
 
-	// reduce Backtracking printing on the log
-	h.Printer = 70
+		var h learning.HyperParameters
+		h.Threads = runtime.NumCPU()
+		h.Factor = difficulty // affects the solution size
 
-	// save any solution to disk
-	h.InitialLimit = 99999999
-	h.SetLogger("solutions.txt")
+		// shuffle before solving attempts
+		h.Shuffle = true
+		h.Seed = true
 
-	h.EndWhenSolved = true
+		// restart when stuck
+		h.DeadlineMs = 1000
+		h.DeadlineRetry = 3
 
-	h.Training(dataset)
+		// affects how fast is the modulo reduced
+		h.Subtractor = difficulty
+
+		// reduce Backtracking printing on the log
+		h.Printer = 70
+
+		// save any solution to disk
+		h.InitialLimit = 1000 + 4*tally.Len()
+		h.EndWhenSolved = true
+
+		h.Name = fmt.Sprint(worst)
+		//h.SetLogger("solutions11.txt")
+
+		//h.AvxLanes = 16
+		//h.AvxSkip = 4
+
+		fmt.Println("hashtron position:", worst, "(job size:", tally.Len(), ")")
+
+		htron, err := h.Training(tally)
+		if err != nil {
+			panic(err.Error())
+		}
+		ptr := net.GetHashtron(worst)
+		*ptr = *htron
+
+		tally.Free()
+		runtime.GC()
+	}
+	evaluate := func() {
+		var percent, errsum atomic.Uint64
+		parallel.ForEach(dataset.Len(), 1000, func(j int) {
+			{
+				var io = dataset.Get(j)
+
+				var predicted = net.Infer2(&io)
+
+				if predicted == io.Output()%net.GetClasses() {
+					percent.Add(1)
+				}
+				errsum.Add(uint64(error_abs(uint32(predicted), uint32(io.Output()))))
+			}
+		})
+		success := 100 * int(percent.Load()) / dataset.Len()
+		println("[success rate]", success, "%", "with", errsum.Load(), "errors")
+
+		if dstmodel == nil || *dstmodel == "" {
+			err := net.WriteZlibWeightsToFile("output." + fmt.Sprint(success) + ".json.t.zlib")
+			if err != nil {
+				println(err.Error())
+			}
+		}
+
+		if dstmodel != nil && len(*dstmodel) > 0 && improved_success_rate < success {
+			if improved_success_rate > 0 {
+				err := net.WriteZlibWeightsToFile(*dstmodel)
+				if err != nil {
+					println(err.Error())
+				}
+			}
+			improved_success_rate = success
+		}
+
+		if success == 100 {
+			if difficulty > 1000 {
+				println("Max accuracy or wrong data. Exiting")
+				os.Exit(0)
+			} else {
+				difficulty++
+			}
+		}
+	}
+	if resume != nil && *resume && dstmodel != nil {
+		net.ReadZlibWeightsFromFile(*dstmodel)
+	}
+	for {
+		shuf := net.Branch(false)
+		evaluate()
+		for worst := 0; worst < len(shuf); worst++ {
+			println("training #", worst, "hastron of", len(shuf), "hashtrons total")
+			trainWorst(shuf[worst])
+			if worst == len(shuf)-2 {
+				evaluate()
+			}
+		}
+	}
+
 }

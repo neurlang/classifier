a74e50f3de8338d2714d01630ca4cce0202e0a50
neurlang <77860779+neurlang@users.noreply.github.com>
2025-07-22 06:14:07 +0200

    Reduce complexity

--- cmd/train_phonemizer_multi/main.go
+++ cmd/train_phonemizer_multi/main.go
@@ -1,25 +1,22 @@
 package main
 
-import "os"
 import "sync/atomic"
-import "fmt"
-import "runtime"
 import "flag"
-import "math/rand"
-import "time"
 
 import "github.com/neurlang/classifier/datasets/phonemizer_multi"
+
 //import "github.com/neurlang/classifier/layer/majpool2d"
 import "github.com/neurlang/classifier/layer/sum"
 import "github.com/neurlang/classifier/layer/sochastic"
+
 //import "github.com/neurlang/classifier/layer/parity"
 import "github.com/neurlang/classifier/layer/crossattention"
 import "github.com/neurlang/classifier/datasets"
-import "github.com/neurlang/classifier/hashtron"
+
 //import "github.com/neurlang/classifier/learning"
-import "github.com/neurlang/quaternary"
 import "github.com/neurlang/classifier/net/feedforward"
 import "github.com/neurlang/classifier/parallel"
+import "github.com/neurlang/classifier/trainer"
 
 func error_abs(a, b uint32) uint32 {
 	if a > b {
@@ -34,7 +31,6 @@ func main() {
 	minpremodulo := flag.Int("minpremodulo", 0, "minpremodulo")
 	maxpremodulo := flag.Int("maxpremodulo", 0, "maxpremodulo")
 	maxdepth := flag.Int("maxdepth", 0, "max training depth")
-	part := flag.Int("part", 0, "train on n/part-th")
 	dstmodel := flag.String("dstmodel", "", "model destination .json.lzw file")
 	flag.Bool("pgo", false, "enable pgo")
 	resume := flag.Bool("resume", false, "resume training")
@@ -61,7 +57,7 @@ func main() {
 	const fanout1 = 24
 	const fanout2 = 1
 	const fanout3 = 8
-	
+
 	var net feedforward.FeedforwardNetwork
 	net.NewLayer(fanout1*fanout2, 0)
 	for i := 0; i < fanout3; i++ {
@@ -76,177 +72,55 @@ func main() {
 	}
 	net.NewCombiner(sochastic.MustNew(fanout1*fanout2, 32, fanout3))
 	net.NewLayer(fanout1*fanout2, 0)
-	net.NewCombiner(sum.MustNew([]uint{fanout1*fanout2}, 0))
+	net.NewCombiner(sum.MustNew([]uint{fanout1 * fanout2}, 0))
 	net.NewLayer(1, 0)
 
-
-	trainWorst := func(worst int) func() {
-		var tally = new(datasets.Tally)
-		tally.Init()
-		tally.SetFinalization(true)
-		if premodulo != nil && *premodulo > 0 {
-			tally.SetGlobalPremodulo(uint32(*premodulo))
-		}
-		if minpremodulo != nil && *minpremodulo > 0 && maxpremodulo != nil && *maxpremodulo > 0 {
-			const span = 50 * 50
-			value := (100 - improved_success_rate) * (100 - improved_success_rate)
-			premodulo := value * ( *minpremodulo - *maxpremodulo ) / span + *maxpremodulo
-			//println(improved_success_rate, premodulo)
-			if premodulo < 2 {
-				premodulo = 2
-			}
-			tally.SetGlobalPremodulo(uint32(premodulo))
-		}
-		var parts = 1
-		if part != nil && *part > 1 {
-			rand.Seed(time.Now().UnixNano())
-			rand.Shuffle(len(data), func(i, j int) { data[i], data[j] = data[j], data[i] })
-			parts = *part
-		}
-		
-		parallel.ForEach(len(data)/parts, 1000, func(jjj int) {
-			for i := 0; i < fanout1; i++ {
-				var sample = data[jjj].V2(fanout1, i)
-				for j := 0; j < sample.Len(); j++ {
-					var io = sample.IO(j)
-
-					net.Tally4(io, worst, tally, nil)
+	trainWorst := trainer.NewTrainWorstFunc(net, minpremodulo, premodulo, maxpremodulo,
+		func(worst []int, tally datasets.AnyTally) {
+			parallel.ForEach(len(data), 1000, func(jjj int) {
+				for i := 0; i < fanout1; i++ {
+					sample := data[jjj].V2(fanout1, i)
+					for j := 0; j < sample.Len(); j++ {
+						io := sample.IO(j)
+						net.AnyTally(io, worst, tally, nil)
+					}
 				}
-			}
+			})
 		})
-		
-		if !tally.GetImprovementPossible() {
-			return nil
-		}
+	evaluate := trainer.NewEvaluateFunc(net, len(data), 99, &improved_success_rate, dstmodel,
+		func(length int, h trainer.EvaluateFuncHasher) int {
+			var percent, errsum, total atomic.Uint64
+			parallel.ForEach(length, 1000, func(j int) {
 
-		fmt.Println("hashtron position:", worst, "(job size:", tally.Len(), ")")
-		ptr := net.GetHashtron(worst)
-		dset := tally.Dataset()
-		q := quaternary.Make(dset)
-		var pmod = [][2]uint32{}
-		if (premodulo != nil && *premodulo > 0) || (minpremodulo != nil && *minpremodulo > 0 && maxpremodulo != nil && *maxpremodulo > 0) {
-			pmod = [][2]uint32{tally.GetGlobalSaltPremodulo()}
-		}
-		htron, err := hashtron.New(pmod, ptr.Bits(), []byte(q))
-		if err != nil {
-			panic(err.Error())
-		}
-		var backup = *ptr
-		*ptr = *htron
+				var pred uint16
 
-		tally.Free()
-		runtime.GC()
+				for i := 0; i < fanout1; i++ {
+					var sample = data[j].V2(fanout1, i)
+					for jj := 0; jj < sample.Len(); jj++ {
+						var io = sample.IO(jj)
 
-		return func(){
-			*ptr = backup
-		}
-	}
-	evaluate := func() (int, [32]byte) {
-		var parts = 1
-		if part != nil && *part > 1 {
-			parts = *part
-		}
-		var h = parallel.NewUint16Hasher(len(data))
-		var percent, errsum, total atomic.Uint64
-		parallel.ForEach(len(data)/parts, 1000, func(j int) {
-		
-			var pred uint16
-		
-			for i := 0; i < fanout1; i++ {
-				var sample = data[j].V2(fanout1, i)
-				for jj := 0; jj < sample.Len(); jj++ {
-					var io = sample.IO(jj)
-				
-					//fmt.Printf("Sample IO %d %d: %v\n", i, jj, sample.IO(jj).SampleSentence.Sample.Sentence)
-				
-				
-					var predicted = net.Infer2(io) & 1
-					
-					pred += predicted
-					
-					if predicted == io.Output() {
-						percent.Add(1)
+						//fmt.Printf("Sample IO %d %d: %v\n", i, jj, sample.IO(jj).SampleSentence.Sample.Sentence)
+
+						var predicted = net.Infer2(io) & 1
+
+						pred += predicted
+
+						if predicted == io.Output() {
+							percent.Add(1)
+						}
+						errsum.Add(uint64(error_abs(uint32(predicted), uint32(io.Output()))))
+						total.Add(1)
 					}
-					errsum.Add(uint64(error_abs(uint32(predicted), uint32(io.Output()))))
-					total.Add(1)
 				}
-			}
-			
-			h.MustPutUint16(j, pred)
+
+				h.MustPutUint16(j, pred)
+			})
+			success := 100 * int(percent.Load()) / int(total.Load())
+			println("[success rate]", success, "%", "with", errsum.Load(), "errors")
+
+			return success
+
 		})
-		success := 100 * int(percent.Load()) / (int(total.Load())/parts)
-		println("[success rate]", success, "%", "with", uint64(parts) * errsum.Load(), "errors")
-
-		if dstmodel == nil || *dstmodel == "" {
-			err := net.WriteZlibWeightsToFile("output." + fmt.Sprint(success) + ".json.t.lzw")
-			if err != nil {
-				println(err.Error())
-			}
-		}
-
-		if dstmodel != nil && len(*dstmodel) > 0 && (improved_success_rate < success || success == 99) {
-			if improved_success_rate > 0 {
-				err := net.WriteZlibWeightsToFile(*dstmodel)
-				if err != nil {
-					println(err.Error())
-				}
-			}
-			improved_success_rate = success
-		}
-
-		if success == 100 {
-			println("Max accuracy or wrong data. Exiting")
-			os.Exit(0)
-		}
-		return success, h.Sum()
-	}
-	if resume != nil && *resume && dstmodel != nil {
-		err := net.ReadZlibWeightsFromFile(*dstmodel)
-		if err != nil {
-			println(err.Error())
-		}
-	}
-	var m = parallel.NewMoveSet()
-	var success, state = evaluate()
-	var default_backoff = func() {
-		println("Infinite loop - algorithm stuck in local minimum. Exiting")
-		os.Exit(0)
-	}
-	backoff := default_backoff
-	var local_minimums = make(map[[32]byte]struct{})
-	fmt.Printf("%x\n", state)
-	for {
-		for infloop := 0; infloop < net.Len(); infloop++ {
-			shuf := net.Branch(false)
-			if m.Exists(state, shuf[0], byte(success)) {
-				continue
-			}
-			for worst := 0; worst < len(shuf) && worst < *maxdepth; worst++ {
-				println("training #", worst, "hastron of", len(shuf), "hashtrons total")
-				if this_backoff := trainWorst(shuf[worst]); this_backoff != nil {
-					infloop = -1
-					this_success, this_state := evaluate()
-					if _, bad := local_minimums[this_state]; bad {
-						this_backoff()
-						break
-					} else {
-						backoff, success, state = this_backoff, this_success, this_state
-					}
-				} else if worst == 0 {
-					break
-				}
-				fmt.Printf("%x\n", state)
-				m.Insert(state, shuf[worst], byte(success))
-				if worst != len(shuf)-1 {
-					if m.Exists(state, shuf[worst+1], byte(success)) {
-						break
-					}
-				}
-			}
-		}
-		local_minimums[state] = struct{}{}
-		backoff()
-		backoff = default_backoff
-		success, state = evaluate()
-	}
+	trainer.Resume(net, resume, dstmodel)
+	trainer.NewLoopFunc(net, &improved_success_rate, 95, evaluate, trainWorst)()
 }

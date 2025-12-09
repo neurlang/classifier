3149b3f1e7c9f7444b979bb860cb44ff707e0c8c
neurlang <77860779+neurlang@users.noreply.github.com>
2025-12-09 19:55:03 +0100

    Revert "refactor: Remove unnecessary nil checks and unused receiver variables"
    This reverts commit a0e45cb8b1f0d56e30afd69183556e8f236a1fbb.

--- datasets/isvirus/virus.go
+++ datasets/isvirus/virus.go
@@ -5,7 +5,7 @@ import "github.com/neurlang/classifier/datasets/stringhash"
 
 type Dataslice struct{}
 
-func (Dataslice) Get(n int) Sample {
+func (_ Dataslice) Get(n int) Sample {
 	if len(virus) <= n {
 		n -= len(virus)
 		return Sample(stringhash.Sample{
@@ -18,7 +18,7 @@ func (Dataslice) Get(n int) Sample {
 		Out: 1,
 	})
 }
-func (Dataslice) Len() int {
+func (_ Dataslice) Len() int {
 	return len(virus) + len(clean)
 }
 
--- hashtron/constructor.go
+++ hashtron/constructor.go
@@ -5,14 +5,14 @@ import "math/rand"
 
 // New creates a new hashtron (giving pseudo random output), or initialized using a provided program and bits
 func New(program [][2]uint32, bits byte, future ...interface{}) (h *Hashtron, err error) {
-	if len(future) > 1 {
+	if future != nil && len(future) > 1 {
 		return nil, errors.New("future arg not supported (new Hashtron)")
 	}
 	h = new(Hashtron)
 	if bits == 0 {
 		bits = 1
 	}
-	if len(program) == 0 {
+	if program == nil {
 		h.program = [][2]uint32{{rand.Uint32() >> 1, 2}}
 	} else {
 		h.program = program
